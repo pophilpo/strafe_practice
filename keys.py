a59f4535273f19531bbc604ea8023decc82ec01e
Popov Philipp <philolippo@gmail.com>
2020-04-13 21:03:12 +0300

    Add strafe key scan codes init

--- keys.py
+++ keys.py
@@ -18,6 +18,8 @@ class KeyListener:
 
         self.stop_key = stop_key
         self.events = list()
+        self.left_strafe = 0
+        self.right_strafe = 0
 
     def listen(self, event):
 
@@ -30,6 +32,25 @@ class KeyListener:
         keyboard.hook(self.listen)
         keyboard.wait(self.stop_key)
 
+    def map_strafe_scan_codes(self):
+        print("\rPress your right strafe key")
+
+        time.sleep(0.5)
+
+        right_strafe_key = keyboard.read_key()
+        right_strafe = keyboard.key_to_scan_codes(right_strafe_key)[0]
+
+        print("\rPress your left strafe key")
+        time.sleep(0.5)
+
+        left_strafe_key = keyboard.read_key()
+        left_strafe = keyboard.key_to_scan_codes(left_strafe_key)[0]
+
+        self.left_strafe = left_strafe
+        self.right_strafe = right_strafe
+
+        print("\rStrafe keys are now mapped")
+
     def __repr__(self):
 
         events = [event.__repr__() for event in self.events]
@@ -40,8 +61,9 @@ class KeyListener:
 def main():
 
     logger = KeyListener()
-    logger.start()
-    print(logger)
+    logger.map_strafe_scan_codes()
+    print(logger.left_strafe)
+    print(logger.right_strafe)
 
 
 if __name__ == "__main__":
